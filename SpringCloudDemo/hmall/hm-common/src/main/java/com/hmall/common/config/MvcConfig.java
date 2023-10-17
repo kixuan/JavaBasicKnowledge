a0c0c8bd58c8674a40b09524b924f18e85fede1b
kixuan <1752845542@qq.com>
2023-10-17 18:24:24 +0800

    feat：SpringCloudDemo 网关登录校验

---
+++ SpringCloudDemo/hmall/hm-common/src/main/java/com/hmall/common/config/MvcConfig.java
@@ -0,0 +1,23 @@
+package com.hmall.common.config;
+
+import com.hmall.common.interceptor.UserInfoInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author kixuan
+ * @version 1.0
+ */
+
+@Configuration
+// 呃呃呃不太懂
+@ConditionalOnClass(WebMvcConfigurer.class)
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserInfoInterceptor())
+                .addPathPatterns("/**");
+    }
+}
