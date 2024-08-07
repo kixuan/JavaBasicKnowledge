c3fefc9fcae1733979c509cdda935e5962a77f6c
kixuan <1752845542@qq.com>
2024-08-07 22:03:47 +0800

    feat:SaToken_demo代码

---
+++ satokenDemo/src/test/java/com/example/satokendemo/SatokenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.satokendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatokenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
